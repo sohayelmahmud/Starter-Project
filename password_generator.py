a3979621f0a22fa1230d83ba98796822fe93329a
sohayelmahmud <sohayelmahmud612@gmail.com>
2025-11-04 15:35:28 +0600

    init

---
+++ password_generator.py
@@ -0,0 +1,14 @@
+import random
+
+characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+
+print("Welcome to our password generator.")
+while True:
+    length = int(input("Input the minimum length of your password: "))
+
+
+    password_chars = [random.choice(characters) for _ in range(length)]
+
+    password = ''.join(password_chars)
+
+    print(password)
