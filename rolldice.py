a09f247c52bf8092ec724464d2c95c891b780deb
sohayelmahmud <sohayelmahmud612@gmail.com>
2025-11-05 20:43:21 +0600

    bug fixed

--- rolldice.py
+++ rolldice.py
@@ -1,11 +1,16 @@
 import random
 
-rolldice = True
-
-while rolldice:
+while True:
     print(random.randint(1, 6))
-    user_input = input("Roll the dice again? (y/n): ").strip().lower()
-    if user_input == 'y':
-        continue
-    else:
-        break
+
+    while True:
+        play_again = input("Do you want to play again? (y/n): ").lower().strip()
+        if play_again == "n":
+            print("Thanks for playing. Goodbye!")
+            exit()
+        elif play_again == "y":
+            print("Starting a new game.")
+            print("......................")
+            break
+        else:
+            print("Invalid Input. please enter 'y' or 'n'.")
