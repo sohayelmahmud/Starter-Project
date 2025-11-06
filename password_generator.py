f338869daa5f5120170a8c55c0215dd82171635a
sohayelmahmud <sohayelmahmud612@gmail.com>
2025-11-06 12:11:53 +0600

    customized

--- password_generator.py
+++ password_generator.py
@@ -42,7 +42,7 @@ while True:
     print(password)
 
     while True:
-        play_again = input("Do you want to generate password? (y/n): ").lower().strip()
+        play_again = input("Do you want to generate another password? (y/n): ").lower().strip()
         if play_again == "n":
             print("Thanks.. Goodbye!")
             exit()
